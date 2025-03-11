26799b2415eed23445f1993ae6c15e198bf59e06
Britney Wang <britney.wang@canonical.com>
2025-03-11 16:27:48 +0800

    Bump package version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.sitemaps-parser",
-    version="1.0.2",
+    version="1.0.3",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical/canonicalwebteam.sitemaps-parser",
